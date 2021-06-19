b3d794678eefc4e6d91260bdb8520ca0d3439cbf
Thai <thainguyen.liv@gmail.com>
2021-06-19 16:38:03 +0700

    update order dto

--- order_dto/order.go
+++ order_dto/order.go
@@ -15,47 +15,49 @@ type Order struct {
 }
 
 type OrderLines struct {
-	OrderLine []struct {
-		LineNumber        string            `json:"lineNumber"`
-		Item              Item              `json:"item"`
-		OrderLineQuantity OrderLineQuantity `json:"orderLineQuantity"`
-		SellerOrderID     *string           `json:"sellerOrderId,omitempty"`
-		OrderLineStatuses struct {
-			OrderLineStatus []struct {
-				Status string `json:"status"`
-				Asn    *struct {
-					PackageASN string  `json:"packageASN"`
-					PalletASN  *string `json:"palletASN,omitempty"`
-				} `json:"asn"`
-				StatusQuantity struct {
-					UnitOfMeasurement string `json:"unitOfMeasurement"`
-					Amount            string `json:"amount"`
-				} `json:"statusQuantity"`
-				TrackingInfo struct {
-					ShipDateTime int64 `json:"shipDateTime"`
-					CarrierName  struct {
-						OtherCarrier *string `json:"otherCarrier,omitempty"`
-						Carrier      *string `json:"carrier,omitempty"`
-					} `json:"carrierName"`
-					MethodCode     string  `json:"methodCode"`
-					TrackingNumber string  `json:"trackingNumber"`
-					TrackingURL    *string `json:"trackingURL,omitempty"`
-				} `json:"trackingInfo"`
-				ReturnCenterAddress *struct {
-					Name       *string `json:"name,omitempty"`
-					Address1   string  `json:"address1"`
-					Address2   *string `json:"address2,omitempty"`
-					City       string  `json:"city"`
-					State      string  `json:"state"`
-					PostalCode string  `json:"postalCode"`
-					Country    string  `json:"country"`
-					DayPhone   *string `json:"dayPhone,omitempty"`
-					EmailID    *string `json:"emailId,omitempty"`
-				} `json:"returnCenterAddress,omitempty"`
-			} `json:"orderLineStatus"`
-		} `json:"orderLineStatuses"`
-		SellerOrderNo *string `json:"sellerOrderNo,omitempty"`
-	} `json:"orderLine"`
+	OrderLine []OrderLine `json:"orderLine"`
+}
+
+type OrderLine struct {
+	LineNumber        string            `json:"lineNumber"`
+	Item              Item              `json:"item"`
+	OrderLineQuantity OrderLineQuantity `json:"orderLineQuantity"`
+	SellerOrderID     *string           `json:"sellerOrderId,omitempty"`
+	OrderLineStatuses struct {
+		OrderLineStatus []struct {
+			Status string `json:"status"`
+			Asn    *struct {
+				PackageASN string  `json:"packageASN"`
+				PalletASN  *string `json:"palletASN,omitempty"`
+			} `json:"asn"`
+			StatusQuantity struct {
+				UnitOfMeasurement string `json:"unitOfMeasurement"`
+				Amount            string `json:"amount"`
+			} `json:"statusQuantity"`
+			TrackingInfo struct {
+				ShipDateTime int64 `json:"shipDateTime"`
+				CarrierName  struct {
+					OtherCarrier *string `json:"otherCarrier,omitempty"`
+					Carrier      *string `json:"carrier,omitempty"`
+				} `json:"carrierName"`
+				MethodCode     string  `json:"methodCode"`
+				TrackingNumber string  `json:"trackingNumber"`
+				TrackingURL    *string `json:"trackingURL,omitempty"`
+			} `json:"trackingInfo"`
+			ReturnCenterAddress *struct {
+				Name       *string `json:"name,omitempty"`
+				Address1   string  `json:"address1"`
+				Address2   *string `json:"address2,omitempty"`
+				City       string  `json:"city"`
+				State      string  `json:"state"`
+				PostalCode string  `json:"postalCode"`
+				Country    string  `json:"country"`
+				DayPhone   *string `json:"dayPhone,omitempty"`
+				EmailID    *string `json:"emailId,omitempty"`
+			} `json:"returnCenterAddress,omitempty"`
+		} `json:"orderLineStatus"`
+	} `json:"orderLineStatuses"`
+	SellerOrderNo *string `json:"sellerOrderNo,omitempty"`
 }
 
 type ShippingInfo struct {
