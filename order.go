ff55de4dcf2290870c544ddd2163e83de3fd4225
Thai <thainguyen.liv@gmail.com>
2021-06-27 16:08:31 +0700

    add func get released order

--- order.go
+++ order.go
@@ -8,15 +8,17 @@ import (
 )
 
 const (
-	AcknowledgeOrderBasePath = "/v3/orders/%s/acknowledge"
-	ShipOrderBasePath        = "/v3/orders/%s/shipping"
-	CancelOrderBasePath      = "/v3/orders/%s/cancel"
+	AcknowledgeOrderBasePath  = "/v3/orders/%s/acknowledge"
+	ShipOrderBasePath         = "/v3/orders/%s/shipping"
+	CancelOrderBasePath       = "/v3/orders/%s/cancel"
+	GetReleasedOrdersBasePath = "/v3/orders/released"
 )
 
 type OrderService interface {
 	Acknowledge(ctx context.Context, purchaseOrderID string) (*order_dto.OrderResponse, error)
 	Ship(ctx context.Context, purchaseOrderID string, payload order_dto.OrderShipmentPayload) (*order_dto.OrderResponse, error)
 	Cancel(ctx context.Context, purchaseOrderID string, payload order_dto.OrderCancellationPayload) (*order_dto.OrderResponse, error)
+	GetReleasedOrders(ctx context.Context, params *order_dto.GetReleasedOrdersParams, nextCursor *string) (*order_dto.ReleasedOrdersResponse, error)
 }
 
 type orderService struct {
@@ -53,6 +55,23 @@ func (s orderService) Cancel(ctx context.Context, purchaseOrderID string, payloa
 		return nil, err
 	}
 	var res order_dto.OrderResponse
+	err = response.Decode(&res)
+	return &res, err
+}
+
+func (s orderService) GetReleasedOrders(ctx context.Context, params *order_dto.GetReleasedOrdersParams, nextCursor *string) (*order_dto.ReleasedOrdersResponse, error) {
+	path := GetReleasedOrdersBasePath
+	var options interface{} = nil
+	if nextCursor != nil && *nextCursor != "" {
+		path = GetReleasedOrdersBasePath + *nextCursor
+		options = params
+	}
+
+	response, err := s.client.Get(ctx, path, options)
+	if err != nil {
+		return nil, err
+	}
+	var res order_dto.ReleasedOrdersResponse
 	err = response.Decode(&res)
 	return &res, err
 }
---
+++ order_dto/get_released_order.go
@@ -0,0 +1,30 @@
+package order_dto
+
+type GetReleasedOrdersParams struct {
+	CreatedStartDate     string `json:"createdStartDate,omitempty"`
+	CreatedEndDate       string `json:"createdEndDate,omitempty"`
+	Limit                string `json:"limit,omitempty"`
+	ProductInfo          string `json:"productInfo,omitempty"`
+	ShipNodeType         string `json:"shipNodeType,omitempty"`
+	Sku                  string `json:"sku,omitempty"`
+	CustomerOrderID      string `json:"customerOrderId,omitempty"`
+	PurchaseOrderID      string `json:"purchaseOrderId,omitempty"`
+	FromExpectedShipDate string `json:"fromExpectedShipDate,omitempty"`
+	ToExpectedShipDate   string `json:"toExpectedShipDate,omitempty"`
+	ShippingProgramType  string `json:"shippingProgramType,omitempty"`
+	ReplacementInfo      string `json:"replacementInfo,omitempty"`
+	OrderType            string `json:"orderType,omitempty"`
+}
+
+type ReleasedOrdersResponse struct {
+	List struct {
+		Meta struct {
+			TotalCount int    `json:"totalCount"`
+			Limit      int    `json:"limit"`
+			NextCursor string `json:"nextCursor"`
+		} `json:"meta"`
+		Elements struct {
+			Order []Order `json:"order"`
+		} `json:"elements"`
+	} `json:"list"`
+}
